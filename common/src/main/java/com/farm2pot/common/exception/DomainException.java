acb891180455ebc0d69fe07d212e999c6666b3a8
Yeon6v6 <merleawe012@gmail.com>
2025-11-16 11:19:16 +0900

    feat: 공통 예외처리 추가

---
+++ common/src/main/java/com/farm2pot/common/exception/DomainException.java
@@ -0,0 +1,14 @@
+package com.farm2pot.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class DomainException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public DomainException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
